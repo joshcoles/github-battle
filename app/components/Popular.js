67e87c55991f09a536241eec71e10bc54eb89613
Josh Coles <josh@joshcoles.com>
2017-05-05 16:27:35 -0700

    Rendered results and fixed issue with api call (turns out it was a typo in getUserData function -_-

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,7 +2,6 @@ var React = require('react');
 var PropTypes = require('prop-types');
 var api = require('../utils/api');
 
-
 function SelectLanguage (props) {
   var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
   return (
@@ -24,16 +23,16 @@ function SelectLanguage (props) {
 function RepoGrid (props) {
   return (
     <ul className='popular-list'>
-      {props.repos.map(function(repo, index) {
+      {props.repos.map(function (repo, index) {
         return (
           <li key={repo.name} className='popular-item'>
             <div className='popular-rank'>#{index + 1}</div>
             <ul className='space-list-items'>
               <li>
                 <img
-                className='avatar'
-                src={repo.owner.avatar_url}
-                alt={'Avatar for ' + repo.owner.login}
+                  className='avatar'
+                  src={repo.owner.avatar_url}
+                  alt={'Avatar for ' + repo.owner.login}
                 />
               </li>
               <li><a href={repo.html_url}>{repo.name}</a></li>
@@ -43,7 +42,6 @@ function RepoGrid (props) {
           </li>
         )
       })}
-
     </ul>
   )
 }
@@ -57,23 +55,19 @@ SelectLanguage.propTypes = {
   onSelect: PropTypes.func.isRequired,
 };
 
-
-
 class Popular extends React.Component {
   constructor(props) {
     super();
     this.state = {
       selectedLanguage: 'All',
-      repos: null
+      repos: null,
     };
 
     this.updateLanguage = this.updateLanguage.bind(this);
   }
-
   componentDidMount() {
-    this.updateLanguage(this.state.selectedLanguage);
+    this.updateLanguage(this.state.selectedLanguage)
   }
-
   updateLanguage(lang) {
     this.setState(function () {
       return {
@@ -83,26 +77,23 @@ class Popular extends React.Component {
     });
 
     api.fetchPopularRepos(lang)
-      .then(function(repos){
-        this.setState(function() {
+      .then(function (repos) {
+        this.setState(function () {
           return {
-            repos: repos,
+            repos: repos
           }
-        })
+        });
       }.bind(this));
-
   }
   render() {
     return (
       <div>
         <SelectLanguage
           selectedLanguage={this.state.selectedLanguage}
-          onSelect={this.updateLanguage}
-          />
-          {!this.state.repos
-            ? <p>LOADING</p>
-            : <RepoGrid repos={this.state.repos} />}
-
+          onSelect={this.updateLanguage} />
+        {!this.state.repos
+          ? <p>LOADING!</p>
+          : <RepoGrid repos={this.state.repos} />}
       </div>
     )
   }
