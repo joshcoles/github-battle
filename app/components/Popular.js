3914d777b29c08fc10f0b6abde8a54dfe968bf6a
Josh Coles <josh@joshcoles.com>
2017-05-08 13:40:51 -0700

    Completed building out components and hosted with firebase. Need to re-visit to add disclaimer that I built it with a tutorial + center that button.

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var api = require('../utils/api');
+var Loading = require('./loading');
 
 function SelectLanguage (props) {
   var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -92,7 +93,7 @@ class Popular extends React.Component {
           selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage} />
         {!this.state.repos
-          ? <p>LOADING!</p>
+          ? <Loading text='DOWLOADING' speed={200} />
           : <RepoGrid repos={this.state.repos} />}
       </div>
     )
